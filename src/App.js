70a1587560913ffd8d695c8a788144e7f36176a7
NadiiaTysh <syao.xiwang@gmail.com>
2024-05-10 19:10:39 -0400

    Final Project done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseItem from './components/ExpenseItem';
+import Currency from './components/Currency';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 
@@ -24,18 +24,21 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
-                    <h3 className='mt-3'>Allocation</h3>
-                    <div className='row'>
-                        <div className='col-sm'>
-                            <ExpenseList />
-                        </div>  
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
-                    <h3 className='mt-3'>Change allocation</h3>
-                    <div className='row mt-3'>
-                        <div className='col-sm'>
-                            <AllocationForm />
-                        </div>  
-                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row'>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>  
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>  
                 </div>
             </div>
         </AppProvider>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,39 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const {
+        dispatch,
+        budget,
+        expenses,
+        currency
+    } = useContext(AppContext);
+    const budgetMax = 20000;
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        let budgetInput = event.target.value;
+        if (budgetInput > budgetMax) {
+            alert ("The value cannot exceed the budget maximum 20,000");
+            budgetInput = budgetMax;
+        }
+        let expensesCost = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+        if (budgetInput < expensesCost) {
+            alert ("You cannot reduce the budget lower than the spending");
+            budgetInput = expensesCost;
+        }
+
+        dispatch({
+            type: "SET_BUDGET",
+            payload: budgetInput,
+        })
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{budget}</span>
+            <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
